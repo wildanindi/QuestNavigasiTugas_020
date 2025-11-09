958d1672304bbab8cce4788c05809eec309ce47a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 20:33:13 +0700

    Membuat scaffold untuk background, menambah title, dan membuat bottom bar untuk tombol Beranda dan Daftar

--- app/src/main/java/com/example/questnavigasitugas_020/view/ListScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/ListScreen.kt
@@ -1,9 +1,23 @@
 package com.example.questnavigasitugas_020.view
 
+import com.example.questnavigasitugas_020.Peserta
+import com.example.questnavigasitugas_020.dummyPesertaList
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasitugas_020.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -12,5 +26,51 @@ fun ListScreen(
     onFormulirClicked: () -> Unit = {},
     onBerandaClicked: () -> Unit = {}
 ) {
+    Scaffold(
+        containerColor = Color.LightGray,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.titleList),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                )
+            )
+        },
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                OutlinedButton(
+                    onClick = onBerandaClicked,
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    shape = RoundedCornerShape(12.dp),
+                ) {
+                    Text(text = stringResource(id = R.string.btnBackList))
+                }
 
+                Button(
+                    onClick = onFormulirClicked,
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    shape = RoundedCornerShape(12.dp),
+                ) {
+                    Text(text = stringResource(id = R.string.btnDaftarList))
+                }
+            }
+        }
+    ) {
+        }
     }
