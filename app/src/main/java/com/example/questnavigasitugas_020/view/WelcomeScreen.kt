490e2ea1b25bd20096e6cd51da0c4ebc5568d304
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 18:49:01 +0700

    Menambah membuat function dan menambah scaffold pada welcomescreen

--- app/src/main/java/com/example/questnavigasitugas_020/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/WelcomeScreen.kt
@@ -1,1 +1,18 @@
 package com.example.questnavigasitugas_020.view
+
+
+import com.example.questnavigasitugas_020.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+
+
+@Composable
+fun WelcomeScreen(
+    onMasukClicked: () -> Unit = {}
+) {
+    Scaffold(
+        containerColor = colorResource(id = R.color.white)
+    ) {
+    }
+}
