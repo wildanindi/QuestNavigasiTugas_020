411214080137d44a0e7d2c7876ff6369f0ae59f5
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 20:47:49 +0700

    Membuat data class untuk peserta, yang berisi valuable id, nama, gender, status, dan alamat

--- app/src/main/java/com/example/questnavigasitugas_020/DataViewModel.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/DataViewModel.kt
@@ -1,1 +1,11 @@
 package com.example.questnavigasitugas_020
+
+
+data class Peserta(
+    val id: Int,
+    val nama: String,
+    val gender: String,
+    val status: String,
+    val alamat: String
+)
+
