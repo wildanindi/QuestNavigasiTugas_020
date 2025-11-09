969dc0357a6112f19c1ea219fa213cc2b5dfa8cb
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 21:19:06 +0700

    Membuat function untuk FormScreen dan menambahkan lambda untuk button Back dan button Submit

--- app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
@@ -1,2 +1,16 @@
 package com.example.questnavigasitugas_020.view
 
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(
+    onBackClicked: () -> Unit = {},
+    onSubmitClicked: (String, String, String, String) -> Unit = { _, _, _, _ -> }
+) {
+
+
+}
