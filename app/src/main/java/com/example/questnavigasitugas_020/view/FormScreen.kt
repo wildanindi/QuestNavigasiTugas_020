38e3881e6064989e5e29f1a30a0740b00df50667
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 21:27:59 +0700

    Menambah scaffold serta menambah text untuk title FormScreen, serta menambahkan string pada resource

--- app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
@@ -1,9 +1,20 @@
 package com.example.questnavigasitugas_020.view
 
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasitugas_020.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,6 +36,23 @@ fun FormScreen(
     var showErrorDialog by remember { mutableStateOf(false) }
 
 
-
-
-}
+    Scaffold(
+        containerColor = Color(0xFFF5F3F8),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.titleForm),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                )
+            )
+        }
+    ) {
+        }
+    }
