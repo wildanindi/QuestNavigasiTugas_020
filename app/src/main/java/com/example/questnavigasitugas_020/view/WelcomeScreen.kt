7833c0a7813e32275efbc8c70701fe64411d8560
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 18:52:06 +0700

    Menambah Column untuk mengatur padding dari komponen yang ada di dalamnya, serta menambah text selamat datang

--- app/src/main/java/com/example/questnavigasitugas_020/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/WelcomeScreen.kt
@@ -2,10 +2,27 @@ package com.example.questnavigasitugas_020.view
 
 
 import com.example.questnavigasitugas_020.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(
@@ -13,6 +30,23 @@ fun WelcomeScreen(
 ) {
     Scaffold(
         containerColor = colorResource(id = R.color.white)
-    ) {
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(horizontal = 32.dp, vertical = 48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.welcome),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+
+
+
+        }
     }
 }
