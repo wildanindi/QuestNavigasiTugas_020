c7655bc3102f641fafede7e620228b8bf62e94a6
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 18:31:50 +0700

    Membuat struktur file dan menambah navigasi tambahan untuk libs dan gradle

---
+++ app/src/main/java/com/example/questnavigasitugas_020/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.questnavigasitugas_020
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questnavigasitugas_020.ui.theme.QuestNavigasiTugas_020Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestNavigasiTugas_020Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DataApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestNavigasiTugas_020Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/questnavigasitugas_020/view/ListScreen.kt
@@ -0,0 +1,1 @@
+package com.example.questnavigasitugas_020.view
