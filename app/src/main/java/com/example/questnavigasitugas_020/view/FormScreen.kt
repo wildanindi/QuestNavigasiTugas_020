a753c881796aecb0f7a7dc3ba11a8b507eb84869
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-09 21:21:36 +0700

    Menambah variabel beserta state untuk dropdown, state untuk showdialog pop up, dan pop up untuk validasi jika data belum di isi nantinya

--- app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/view/FormScreen.kt
@@ -11,6 +11,20 @@ fun FormScreen(
     onBackClicked: () -> Unit = {},
     onSubmitClicked: (String, String, String, String) -> Unit = { _, _, _, _ -> }
 ) {
+    var namaLengkap by remember { mutableStateOf("") }
+    var selectedGender by remember { mutableStateOf("") }
+    var selectedStatus by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+
+    var isStatusDropdownExpanded by remember { mutableStateOf(false) }
+    val statusOptions = listOf("Belum Kawin", "Kawin", "Cerai")
+    val genderOptions = listOf("Laki-laki", "Perempuan")
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    var showErrorDialog by remember { mutableStateOf(false) }
+
+
 
 
 }
