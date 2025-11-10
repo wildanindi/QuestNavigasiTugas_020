44d62a531a094894fe802eea07c5207a87662fe5
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-11-10 07:11:39 +0700

    Membuat enum class Navigasi, dan function DataApp untuk di panggil di MainActivity nantinya

--- app/src/main/java/com/example/questnavigasitugas_020/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_020/Navigasi.kt
@@ -1,1 +1,20 @@
 package com.example.questnavigasitugas_020
+
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+enum class Navigasi {
+    Welcome,
+    Form,
+    List
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+}
